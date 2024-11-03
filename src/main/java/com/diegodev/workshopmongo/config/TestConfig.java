b7ced165f10b5ff34a7c9cf204aa184d6bf25d2e
Diegodamata <Diegodamata>
2024-11-03 18:54:49 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/diegodev/workshopmongo/config/TestConfig.java
+++ src/main/java/com/diegodev/workshopmongo/config/TestConfig.java
@@ -1,12 +1,16 @@
 package com.diegodev.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.diegodev.workshopmongo.domain.Post;
 import com.diegodev.workshopmongo.domain.User;
+import com.diegodev.workshopmongo.repository.PostRepository;
 import com.diegodev.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,20 +18,30 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		//porque estou deletendo? pois como eu criei manualmente 2 usuarios no mongo preciso limpar o mongo
 		//para armazenar os novos usuarios, com base em um arquivo para instanciar os obj
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!", maria);
 		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
